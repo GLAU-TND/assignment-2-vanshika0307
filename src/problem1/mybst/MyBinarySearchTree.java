dd5b2bba70552b0a7f5065df7ccc5b57425ad904
Vanshika Agrawal <vanshika.agrawal_cs18@gla.ac.in>
2020-03-27 22:47:44 +0530

    created root field

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,17 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    TreeNode root;
+    public MyBinarySearchTree
+
+    {
+        root == null;
+    }
+
+
 }
