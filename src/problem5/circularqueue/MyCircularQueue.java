c5b217a37927d6303d1e11fc5c7963419fb6ffd8
Vanshika Agrawal <vanshika.agrawal_cs18@gla.ac.in>
2020-03-28 18:46:43 +0530

    created traverse queue method

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -39,4 +39,27 @@ public class MyCircularQueue {
             setRear(getRear().getNext());
         }
     }
+
+    public void enQueue(Node newNode) {
+        if (getFront() == null && getRear() == null) {
+            setFront(newNode);
+            setRear(newNode);
+            getRear().setNext(getFront());
+        } else {
+            newNode.setNext(getFront());
+            getRear().setNext(newNode);
+            setRear(getRear().getNext());
+        }
+    }
+
+    public void traverseQueue() {
+        Node temp = getFront();
+
+        while (true) {
+            System.out.println(temp.getStudent());
+            temp = temp.getNext();
+            if (temp == getFront())
+                break;
+        }
+    }
 }
