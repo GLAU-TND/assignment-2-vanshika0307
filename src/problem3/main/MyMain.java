70c0552fcbb62702e18e677bb9482b419215fc6e
Vanshika Agrawal <vanshika.agrawal_cs18@gla.ac.in>
2020-03-28 18:05:55 +0530

    created main method

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,18 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+import problem3.myqueue.MyPriorityQueue;
+import problem3.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue<Student> obj = new MyPriorityQueue<>();
+        obj.add(new Student(1, "Ram"));
+        obj.add(new Student(10, "Shyam"));
+        obj.add(new Student(5, "Kartik"));
+        obj.add(new Student(2, "Shashank"));
+        System.out.println(obj);
+    }
 }
